3059c0915f0d28bb204c3cc68af4bee286010acc
Davonne <davonnejv@gmail.com>
2025-11-07 02:01:38 -0600

    update: hero section to full screen

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -2,18 +2,28 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="flex flex-col justify-center items-center shadow-md">
+    <section className="relative flex justify-center items-center w-full h-screen overflow-hidden">
       <Image
         src="/images/tenders.jpg"
         alt="Chicken tenders and fries"
-        width={980}
-        height={980}
-        className="object-cover relative mt-8 rounded"
+        fill
+        className="object-cover"
+        priority
       />
 
-      <p className="absolute top-60 rounded font-serif text-white text-4xl font-bold bg-black p-4">
-        Did someone say...Tenders?
-      </p>
+      <div className="absolute inset-0 bg-black/40"></div>
+
+      <div className="absolute text-center px-4 py-6 rounded bg-black">
+        <p className="text-5xl lg:text-7xl font-bold p-4  text-white drop-shadow-lg">
+          Did someone say...
+        </p>
+        <p
+          className="mt-4 text-6xl lg:text-8xl text-red-600 drop-shadow-2xl"
+          style={{ fontFamily: "var(--font-slackey)" }}
+        >
+          Tenders?!
+        </p>
+      </div>
     </section>
   );
 }
