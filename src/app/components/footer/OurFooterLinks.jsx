ae219e267d9e019728add778ce3b26b4ab79e931
Davonne Vigil <davonnejv@gmail.com>
2025-11-10 22:45:27 -0600

    feature: footer links with responsiveness

--- src/app/components/footer/OurFooterLinks.jsx
+++ src/app/components/footer/OurFooterLinks.jsx
@@ -1,13 +1,23 @@
-import { footerLinks } from "./FooterLinks";
+import { footerLinks } from "./footerLinks";
+import { importantLinks } from "./footerLinks";
 
 export default function OurFooterLinks() {
   return (
-    <ul className="flex flex-col lg:flex-row gap-8">
-      {footerLinks.map((link) => (
-        <li key={link.id} className="font-bold">
-          {link.name}
-        </li>
-      ))}
-    </ul>
+    <section className="grid grid-cols-2 lg:grid-cols-1 gap-4">
+      <ul className="flex flex-col lg:flex-row gap-8">
+        {footerLinks.map((link) => (
+          <li key={link.id} className="text-xl font-bold">
+            {link.name}
+          </li>
+        ))}
+      </ul>
+      <ul className="flex flex-col lg:flex-row gap-8 md:mt-0">
+        {importantLinks.map((link) => (
+          <li key={link.id} className="font-thin text-lg">
+            {link.name}
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
--- src/app/components/footer/footerLinks.js
+++ src/app/components/footer/footerLinks.js
@@ -4,3 +4,11 @@ export const footerLinks = [
   { name: "Feedback", id: 2 },
   { name: "Corporate", id: 3 },
 ];
+
+export const importantLinks = [
+  { name: "Terms & Services", id: 0 },
+  { name: "Privacy Policy", id: 1 },
+  { name: "Corporate Careers", id: 2 },
+  { name: "Reviews", id: 3 },
+  { name: "Franchising", id: 4 },
+];
